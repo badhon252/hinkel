3588b7f0bcaa2cf8a7be407a528c1d3edf68ab31
badhon252 <khalidhossainbadhon@gmail.com>
2026-02-28 04:06:18 +0600

    feat: increase global rate limit maximum requests to 500

--- src/lib/limit.js
+++ src/lib/limit.js
@@ -2,15 +2,13 @@ import rateLimit from 'express-rate-limit';
 
 const globalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 500
 });
 
 const emailVerificationLimiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   max: 5,
-  keyGenerator: (req) => req.body.email,
+  keyGenerator: (req) => req.body.email
 });
 
-
 export { globalLimiter, emailVerificationLimiter };
-
